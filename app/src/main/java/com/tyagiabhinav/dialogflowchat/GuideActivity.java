bdb4667eec8ac97bbea10a2c4b44463a86b08e53
Grace <khr6575@naver.com>
2021-06-11 14:50:52 +0900

    first

--- app/src/main/java/com/tyagiabhinav/dialogflowchat/GuideActivity.java
+++ app/src/main/java/com/tyagiabhinav/dialogflowchat/GuideActivity.java
@@ -2,10 +2,12 @@ package com.tyagiabhinav.dialogflowchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.tyagiabhinav.dialogflowchatlibrary.Chatbot;
 import com.tyagiabhinav.dialogflowchatlibrary.ChatbotActivity;
@@ -15,23 +17,24 @@ import com.tyagiabhinav.dialogflowchatlibrary.DialogflowCredentials;
 import java.util.UUID;
 
 
-public class MainActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_guide);
 
         Button chatFab = findViewById(R.id.chatFab);
         chatFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), GuideActivity.class); startActivity(intent);
+                openChatbot(view);
             }
         });
 
 
     }
+
 
     public void openChatbot(View view) {
         // provide your Dialogflow's Google Credential JSON saved under RAW folder in resources
@@ -43,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 //                .setChatUserAvatar(getDrawable(R.drawable.avatarUser)) // provide avatar for your the user if default is not required
                 .setShowMic(true) // False by Default, True if you want to use Voice input from the user to chat
                 .build());
-        Intent intent = new Intent(MainActivity.this, ChatbotActivity.class);
+        Intent intent = new Intent(GuideActivity.this, ChatbotActivity.class);
         Bundle bundle = new Bundle();
 
         // provide a UUID for your session with the Dialogflow agent
@@ -52,4 +55,26 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtras(bundle);
         startActivity(intent);
     }
+
+
+    public void gwnuButton(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gwnu.ac.kr/sites/kor/index.do"));
+        startActivity(intent);
+    }
+
+    public void intraButton(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://portal.gwnu.ac.kr/portal/default/stu_main"));
+        startActivity(intent);
+    }
+
+    public void echamButton(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://lms.gwnu.ac.kr/"));
+        startActivity(intent);
+    }
+
+    public void libButton(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://iskul.gwnu.ac.kr/wonju/"));
+        startActivity(intent);
+    }
+
 }
